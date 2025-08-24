cfbf73611e69c2e243a3b436edeb2c946a37fccd
cacapon <cacapon.project@gmail.com>
2025-08-24 16:46:25 +0900

    add user free

--- cublibx/srcs/public/cublx_del.c
+++ cublibx/srcs/public/cublx_del.c
@@ -6,13 +6,43 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 07:11:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/08/22 20:50:18 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/08/24 16:44:27 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cublx.h"
 #include <stdlib.h>
 
+static void	_free_cublx_user(t_cub_user **user)
+{
+	if (!user || !*user)
+		return ;
+	free(*user);
+	*user = NULL;
+}
+
+static void	_free_mlx_win(void *mlx, void **win)
+{
+	if (!mlx || !win || !*win)
+		return ;
+	mlx_destroy_window(mlx, *win);
+	*win = NULL;
+}
+
+static void	_free_mlx(void **mlx)
+{
+	if (!mlx || !*mlx)
+		return ;
+	mlx_destroy_display(*mlx);
+	free(*mlx);
+	*mlx = NULL;
+}
+/**
+ * @brief Delete the cublx instance.
+ * 
+ * @param culbx 
+ * @return int 
+ */
 int	cublx_del(t_cublx **culbx)
 {
 	t_cublx	*_cublx;
@@ -20,13 +50,10 @@ int	cublx_del(t_cublx **culbx)
 	if (!culbx || !*culbx)
 		return (1);
 	_cublx = *culbx;
-	if (_cublx->win)
-		mlx_destroy_window(_cublx->mlx, _cublx->win);
-	if (_cublx->mlx)
-	{
-		mlx_destroy_display(_cublx->mlx);
-		free(_cublx->mlx);
-	}
+	_cublx->user->destructor(_cublx);
+	_free_cublx_user(&_cublx->user);
+	_free_mlx_win(_cublx->mlx, &_cublx->win);
+	_free_mlx(&_cublx->mlx);
 	free(_cublx);
 	*culbx = NULL;
 	return (0);
