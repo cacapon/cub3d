ec5a004b31fee76bbb9d2e45d6e9893a3460a89c
cacapon <cacapon.project@gmail.com>
2025-08-26 10:52:51 +0900

    viewのallocateの指定ミスを修正(画面バグの原因)

--- cublibx/srcs/public/cublx_new.c
+++ cublibx/srcs/public/cublx_new.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 07:04:18 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/08/26 10:30:04 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/08/26 10:51:20 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ t_cublx	*cublx_new(int w, int h, char *title)
 		return (cublx_del(&cublx), NULL);
 	if (_allocate_cublx_user(cublx) != 0)
 		return (cublx_del(&cublx), NULL);
-	if (_allocate_views(cublx, h, w) != 0)
+	if (_allocate_views(cublx, w, h) != 0)
 		return (cublx_del(&cublx), NULL);
 	cublx->btn = _cublx_btn;
 	cublx->btnp = _cublx_btnp;
