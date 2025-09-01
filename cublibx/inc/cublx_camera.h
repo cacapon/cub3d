5f55d4bad5407f08b76afa0631ff81fc89f1102f
cacapon <cacapon.project@gmail.com>
2025-09-01 18:19:33 +0900

    add camera

---
+++ cublibx/inc/cublx_camera.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cublx_camera.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 13:19:18 by ttsubo            #+#    #+#             */
+/*   Updated: 2025/09/01 18:18:44 by ttsubo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBLX_CAMERA_H
+# define CUBLX_CAMERA_H
+
+# include "cublx_vec2.h"
+# ifndef M_PI
+#  define M_PI 3.14159265358979323846
+# endif
+
+typedef struct s_cublx_camera	t_camera;
+typedef t_cublx_vec2			t_vec2;
+
+typedef struct s_cublx_camera
+{
+	t_vec2	pos;
+	t_vec2	dir;
+	t_vec2	plane;
+	double	fov_deg;
+
+	t_vec2	(*get_ray)(t_camera * self, int x, int w);
+	void	(*rotate)(t_camera *self, double angle);
+	void	(*move)(t_camera *self, t_vec2 delta);
+	void	(*set_fov)(t_camera *self, double fov_deg);
+}			t_cublx_camera;
+
+// public
+t_camera	*cublx_camera_new(t_vec2 pos, t_vec2 dir, double fov_deg);
+int			cublx_camera_del(t_cublx_camera **camera);
+
+// methods
+t_vec2		_cublx_camera_get_ray(t_camera *self, int x, int w);
+void		_cublx_camera_move(t_camera *self, t_vec2 delta);
+void		_cublx_camera_rotate(t_camera *self, double angle);
+void		_cublx_camera_set_fov(t_camera *self, double fov_deg);
+
+#endif
