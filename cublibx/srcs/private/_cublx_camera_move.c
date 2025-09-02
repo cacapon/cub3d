6c1efedb7f79a89de0c56ee810268975c2ed6297
cacapon <cacapon.project@gmail.com>
2025-09-02 10:26:42 +0900

    カメラの方向に向かって移動できるように修正

--- cublibx/srcs/private/_cublx_camera_move.c
+++ cublibx/srcs/private/_cublx_camera_move.c
@@ -6,14 +6,20 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 13:36:14 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/09/01 18:17:50 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/09/02 10:25:46 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cublx_camera.h"
 
+/**
+ * @brief Move by delta in the direction of the camera.
+ * 
+ * @param self 
+ * @param delta 
+ */
 void	_cublx_camera_move(t_camera *self, t_vec2 delta)
 {
-	self->pos.x += delta.x;
-	self->pos.y += delta.y;
+	self->pos.x += -delta.y * self->dir.x + delta.x * self->plane.x;
+	self->pos.y += -delta.y * self->dir.y + delta.x * self->plane.y;
 }
