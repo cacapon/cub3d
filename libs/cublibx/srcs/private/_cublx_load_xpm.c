94b923f09fe499938180ccfcec38554fa23fbe87
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-09-09 16:19:45 +0900

    fix cublibx

--- libs/cublibx/srcs/private/_cublx_load_xpm.c
+++ libs/cublibx/srcs/private/_cublx_load_xpm.c
@@ -6,11 +6,12 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 18:32:04 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/09/08 23:17:13 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/09/09 16:06:10 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cublx.h"
+#include <stdlib.h>
 
 // error_exit(data, fd, "Failed to allocate memory.");
 // error_exit(data, fd, "Failed to load texture.");
@@ -26,12 +27,17 @@
  */
 int	_cublx_load_xpm(t_cublx *self, t_cublx_img *img, char *xpm_path)
 {
+	char	*trim_path;
 	void	*(*xpm_to_img)(void *mlx, char *fname, int *w, int *h);
 	char	*(*get_addr)(void *img, int *bpp, int *line_h, int *endian);
 
+	trim_path = cublx_strtrim(xpm_path, " \r\t\n");
+	if (!trim_path)
+		return (1);
 	xpm_to_img = mlx_xpm_file_to_image;
 	get_addr = mlx_get_data_addr;
-	img->img = xpm_to_img(self->mlx, xpm_path, &img->size.x, &img->size.y);
+	img->img = xpm_to_img(self->mlx, trim_path, &img->size.x, &img->size.y);
+	free(trim_path);
 	if (!img->img)
 		return (1);
 	img->addr = get_addr(img->img, &img->bits_per_pixel,
